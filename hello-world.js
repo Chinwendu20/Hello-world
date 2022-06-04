3321981956f08fc5d52284a6ccfecdb730b0b803
Maureen Ononiwu <59079323+Chinwendu20@users.noreply.github.com>
2022-06-04 05:41:22 +0100

    Created hello world node app

---
+++ hello-world.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv') 
+const http = require('http');
+dotenv.config()
+
+const hostname = process.env.hostname;
+const port = process.env.PORT;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
